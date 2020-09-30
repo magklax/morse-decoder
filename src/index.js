efc26e36e82c1889df3fd10ab4858131dde45fb8
Nadia <nadic206@yandex.ru>
2020-09-30 09:36:50 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let char = '';
+  let result = '';
+  
+  for (let i = 0; i < expr.length / 10; i++) {
+    char = expr.slice(i * 10, (i  + 1) * 10).replace(/^[0]+/g, '');
+
+    char = char.replace(/10/g, '.').replace(/11/g, '-').replace(/[\*]+/g, ' ');
+
+    if (char === ' ') {
+      result += char;
+    } else {
+      result += MORSE_TABLE[char];
+    }
+  }
+  return result;
 }
 
 module.exports = {
